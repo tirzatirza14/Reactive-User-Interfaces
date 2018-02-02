c565579654ec8f44edeffaa85dd4c56e07b8f9ea
tirzatirza14 <tirza.alberta@nyu.edu>
2018-02-02 17:51:17 +0800

    Combined the date, article, and image sections of the article into one component (Box). Replaced placeholder images and texts with the ones intended for submission.
    Combined the date, article, and image sections of the article into one component (Box). Replaced placeholder images and texts with the ones intended for submission.

--- week-2-app/src/App.js
+++ week-2-app/src/App.js
@@ -3,45 +3,51 @@ import React, {
 } from 'react';
 import './App.css';
 import Box from './Box.js'
-import DateBox from './Date.js'
-import ImageBox from './ImageBox.js'
-
-
+import PlacehImg1 from './assets/1.png';
+import PlacehImg2 from './assets/2.png';
+import PlacehImg3 from './assets/3.png';
 
 class App extends Component {
   render() {
-      const titleText = "A Really Long Title Just To See What It's Like When the Title is Really Long";
-      const bodyText = "Bacon ipsum dolor amet biltong turducken ball tip sausage swine burgdoggen sirloin bresaola t-bone jowl tenderloin pork loin. Pork jerky venison, pastrami tenderloin ham hock brisket fatback tongue ball tip burgdoggen shank. Bacon brisket corned beef swine salami. Ground round chicken venison kevin corned beef flank capicola. Swine frankfurter tri-tip short ribs burgdoggen, spare ribs doner tail ball tip pastrami brisket bacon landjaeger sausage porchetta.";
-      const authorText = "John McCawidule"
+
+      const dateFill = "Jan. 30, 2018";
+
+      const titleText1 = "Kodak's Dubious Blockchain Gamble";
+      const bodyText1 = "What's a 130-year-old photo company doing dabbling in cryptocurrency? Either revolutionising digital rights management or trying to make a quick buck.";
+      const authorText1 = "Kevin Roose"
+
+      const titleText2 = "Taiwan Retaliates Against Chinese Airlines, Hampering Lunar New Year Travel";
+      const bodyText2 = "Taiwan, pushing back over encroachment on Taiwan Strait airspace, may leave thousands without flights home for the holiday.";
+      const authorText2 = "Chris Horton"
+
+      const titleText3 = "New Jersey Embraces an Idea it Once Rejected: Make Utilities Pay to Emit Carbon";
+      const bodyText3 = "Democratic governors nationwide are taking steps to tax or price emissions within their own borders, even as Trump dismantles federal climate policy.";
+      const authorText3 = "Brad Plumer"
 
       return (
-        <div className = "App" >        
-         <div className = "Row">
-          <DateBox datetext = "Jan. 30, 2018" />
+        <div className = "App" >
+          <div className = "Row">
           <Box
-          boxTitle = {titleText}
-          boxText = {bodyText}
-          boxAuthor = {authorText}/>
-          <ImageBox imageBoxText = "image goes here"/>
-         </div>
+          datetext = {dateFill}
+          boxTitle = {titleText1}
+          boxText = {bodyText1}
+          boxAuthor = {authorText1}
+          imgFill = {PlacehImg1}/>
 
-         <div className = "Row">
-          <DateBox datetext = "Jan. 30, 2018" />
           <Box
-          boxTitle = {titleText}
-          boxText = {bodyText}
-          boxAuthor = {authorText}/>
-          <ImageBox imageBoxText = "image goes here"/>
-         </div>
+          datetext = {dateFill}
+          boxTitle = {titleText2}
+          boxText = {bodyText2}
+          boxAuthor = {authorText2}
+          imgFill = {PlacehImg2}/>
 
-         <div className = "Row">
-          <DateBox datetext = "Jan. 30, 2018" />
           <Box
-          boxTitle = {titleText}
-          boxText = {bodyText}
-          boxAuthor = {authorText}/>
-          <ImageBox imageBoxText = "image goes here"/>
-         </div>
+          datetext = {dateFill}
+          boxTitle = {titleText3}
+          boxText = {bodyText3}
+          boxAuthor = {authorText3}
+          imgFill = {PlacehImg3}/>
+          </div>
         </div>
       );
     }
--- week-2-app/src/Box.js
+++ week-2-app/src/Box.js
@@ -5,10 +5,21 @@ class Box extends Component {
   render() {
     return (
       <div className="Box">
-        <p className = "ArtTitle">{this.props.boxTitle}</p>
-        <p className = "ArtBody">{this.props.boxText}</p>
-        <div className = "ArtAuthor">
           <p>By</p><p className = "AuthorName">{this.props.boxAuthor}</p>
+      <div className = "Article">
+        <div className ="DateBox">
+          <p>{this.props.datetext}</p>
+        </div>
+        <div className ="MiddleBox">
+          <p className = "ArtTitle">{this.props.boxTitle}</p>
+          <p className = "ArtBody">{this.props.boxText}</p>
+          <div className = "ArtAuthor">
+            <p>By</p><p className = "AuthorName">{this.props.boxAuthor}</p>
+          </div>
+        </div>
+        <div className = "ImgContainer">
+          <img src={this.props.imgFill}/>
+        </div>
         </div>
       </div>
     );
